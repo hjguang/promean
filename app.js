5acf99a4e6da47ce9e3a5b9ae52a0b84b4f3b501
theseus <houjguang@163.com>
2020-09-06 22:31:50 +0800

    添加rest api

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//先加载
+require('./app_api/models/db');
 
 var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-require('./app_server/models/db');
+var apiRouter = require('./app_api/routes/index');
+
 
 var app = express();
 
@@ -21,7 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
